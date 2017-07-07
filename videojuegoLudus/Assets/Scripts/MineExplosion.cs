299e141b2e866f2e44b3ea4ddb35a2853e8e59ff
RubenCrispin <ruben.crispin@ludus-vr.com>
2017-07-07 08:32:16 +0200

    Minas con explosion

---
+++ videojuegoLudus/Assets/Scripts/MineExplosion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+namespace videojuegoLudus {
+    public class MineExplosion : NetworkBehaviour {
+        public GameObject explosion1;
+        // Use this for initialization
+        void Start() {
+            explosion1.SetActive(true);
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
+
