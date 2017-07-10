390d31346aa9dd0b7318e1cc4e6f8740eff69b28
RubenCrispin <ruben.crispin@ludus-vr.com>
2017-07-10 14:13:12 +0200

    Early Access 1.1.2 - Falta sincronizar los enemigos por Networking

---
+++ videojuegoLudus/Assets/Scripts/Teleport.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+    public Vector3 destination = new Vector3(0, 0, 0);
+    private GameObject[] players;
+    private int i = 0;
+	public void TeleportOutside () {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        while (i < players.Length) {
+            players[i].transform.position = destination;
+            i++;
+        }
+	}
+}
